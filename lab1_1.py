b9bbea45a4c5ea4a14387e544dd6fa60b61a16fd
Artem Yarmosh <artem.yarmosh@hiqo-solutions.com>
2019-10-14 11:42:20 +0300

    fix labs

--- lab1_1.py
+++ lab1_1.py
@@ -3,15 +3,40 @@ import numpy as np
 
 import generators
 
-from math import sqrt
+from math import sqrt, log
+from scipy.stats import chi2
 
-COUNT_NUMBER = 10_000
-INTERVAL_COUNT = l = sqrt(COUNT_NUMBER) if COUNT_NUMBER <= 100 else 1 + log(COUNT_NUMBER, 2)
+
+COUNT_NUMBER = 1_000_000
+INTERVAL_COUNT = l = int(sqrt(COUNT_NUMBER) if COUNT_NUMBER <= 100 else 1 + log(COUNT_NUMBER, 2))
 
 
 MATHEMATICAL_EXPECTATION = 1 / 2 
 DESPERSION = 1 / 12
 
+def pearson_consent(a, b, count, values):
+    intervals = [[i, []] for i in np.arange(a, b, (b-a)/INTERVAL_COUNT)] + [[float(b), []]]
+    
+    for value in random_values:
+        for i in range(len(intervals) - 1):
+            if value >= intervals[i][0] and value < intervals[i+1][0]:
+                intervals[i][-1].append(value)
+                break
+    
+    return intervals
+
+
+def chi_2(interval, theory_func, item_count):
+    result = 0
+    for i, item in enumerate(interval[:-1:]):
+        o = len(item[-1]) / item_count
+        
+        temp = item[0] + interval[i+1][0] / 2
+        e = theory_func(temp)
+
+        result += (o + e)**2 / e
+
+    return result
 
 if __name__ == '__main__':
     generator = generators.BaseRandomGeneretor()
@@ -40,4 +65,10 @@ if __name__ == '__main__':
     R = 12 / (COUNT_NUMBER - step) * pair_sum
     print(R)
 
-    a, b = 0, 0
+    intervals = pearson_consent(0, 1, INTERVAL_COUNT, random_values)
+    
+    chi_2_value = chi_2(intervals, lambda x: COUNT_NUMBER / INTERVAL_COUNT / COUNT_NUMBER, COUNT_NUMBER) 
+    print(chi_2_value)
+
+    l = chi2.pdf(0.99, INTERVAL_COUNT-1)
+    print(l)
--- lab1_2.py
+++ lab1_2.py
@@ -4,7 +4,10 @@ import numpy as np
 
 from math import exp, pi, sqrt, log
 from generators import ContinuousRandomNumberGenerator       
+
 from scipy.optimize import fmin_l_bfgs_b
+from scipy.stats import chi2
+
 
 A_PARAM = 2
 def rayleign_distribution(x):
@@ -26,6 +29,30 @@ def get_end_point_for_y(func, a, b):
                                 (b - a) / 2, bounds=[(a,b)],
                                 approx_grad=True)[0][0]
     return func(x_for_max_y)
+
+def pearson_consent(a, b, count, values):
+    intervals = [[i, []] for i in np.arange(a, b, (b-a)/count)] + [[float(b), []]]
+    
+    for value in values:
+        for i in range(len(intervals) - 1):
+            if value >= intervals[i][0] and value < intervals[i+1][0]:
+                intervals[i][-1].append(value)
+                break
+    
+    return intervals
+
+
+def chi_2(interval, theory_func, item_count):
+    result = 0
+    for i, item in enumerate(interval[:-1:]):
+        o = len(item[-1]) / item_count
+        
+        temp = item[0] + interval[i+1][0] / 2
+        e = theory_func(temp)
+
+        result += (o + e)**2 / e
+
+    return result
 
 if __name__ == '__main__':
     generator = ContinuousRandomNumberGenerator(rayleign_distribution)
@@ -55,3 +82,9 @@ if __name__ == '__main__':
     plt.show()
 
     l = sqrt(COUNT) if COUNT <= 100 else 1 + log(COUNT, 2)
+
+
+    intervals = pearson_consent(A, b, l, values)
+
+    chi_2_value = chi_2(intervals, rayleign_distribution, COUNT)
+    print(chi_2_value)
